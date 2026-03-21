ce86d8dee16f371ded25539ee48b73dbe2ce2ecf
Kelvyn <kelvyn.kaua9@gmail.com>
2026-03-21 19:45:01 -0300

    Fist commit and UserEntity

---
+++ src/main/java/com/core/AuthCore/domain/entity/UserEntity.java
@@ -0,0 +1,12 @@
+package com.core.AuthCore.domain.entity;
+
+import java.util.UUID;
+
+public class UserEntity {
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+}
